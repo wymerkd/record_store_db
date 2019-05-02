ea856f159ba5acc3f5d149d25de7d2b7393ef932
Franz Knupfer <franzknupfer@hotmail.com>
2019-05-02 08:52:57 -0700

    all business logic for record store

---
+++ lib/album.rb
@@ -0,0 +1,42 @@
+class Album
+  attr_accessor :name
+  attr_reader :id
+
+  @@albums = {}
+  @@total_rows = 0
+
+  def initialize(name, id)
+    @name = name
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@albums.values()
+  end
+
+  def save
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def ==(album_to_compare)
+    self.name() == album_to_compare.name()
+  end
+
+  def self.clear
+    @@albums = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@albums[id]
+  end
+
+  def update(name)
+    self.name = name
+    @@albums[self.id] = Album.new(self.name, self.id)
+  end
+
+  def delete
+    @@albums.delete(self.id)
+  end
+end
