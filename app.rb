f3ab29933377dc89327bbd5cf0cc3504fdc6fc7b
Franz Knupfer <franzknupfer@hotmail.com>
2019-05-08 10:50:08 -0700

    add all routes for songs

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/album')
+require('./lib/song')
 require('pry')
 also_reload('lib/**/*.rb')
 
@@ -47,21 +48,31 @@ delete ('/albums/:id') do
 end
 
 get ('/albums/:id/songs') do
-
+  @album = Album.find(params[:id].to_i())
 end
 
-get ('/albums/:id/songs/:id') do
-
+get ('/albums/:id/songs/:song_id') do
+  @song = Song.find(params[:song_id].to_i())
+  erb(:song)
 end
 
 post ('/albums/:id/songs') do
-
+  @album = Album.find(params[:id].to_i())
+  song = Song.new(params[:song_name], @album.id, nil)
+  song.save()
+  erb(:album)
 end
 
-patch ('/albums/:id/songs/:id') do
-
+patch ('/albums/:id/songs/:song_id') do
+  @album = Album.find(params[:id].to_i())
+  @song = Song.find(params[:song_id].to_i())
+  @song.update(params[:name], @album.id)
+  redirect to("/albums/#{@album.id}")
 end
 
-delete ('/albums/:id/songs/:id') do
-
+delete ('/albums/:id/songs/:song_id') do
+  @song = Song.find(params[:song_id].to_i())
+  @song.delete
+  @album = Album.find(params[:id].to_i())
+  erb(:album)
 end
